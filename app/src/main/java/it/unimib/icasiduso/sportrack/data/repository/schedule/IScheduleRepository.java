8458745889ec7757d77ecbf91fbfd6628df6ca33
FerrarioChristian <chri.fer.emi@gmail.com>
2024-08-23 11:34:26 +0200

    Restored (almost) ScheduleFragment functionality  * Implemented ScheduleViewModel and Factory  * Fixed repository and datasource  * Added getScheduleRepository to ServiceLocator  * Fixed ScheduleRecyclerViewAdapter  * Implemented ViewModel in ScheduleFragment

---
+++ app/src/main/java/it/unimib/icasiduso/sportrack/data/repository/schedule/IScheduleRepository.java
@@ -0,0 +1,19 @@
+package it.unimib.icasiduso.sportrack.data.repository.schedule;
+
+
+import java.util.List;
+
+import it.unimib.icasiduso.sportrack.model.schedule.Schedule;
+
+public interface IScheduleRepository {
+
+    void getSchedules(String userId, ScheduleCallback callback);
+    void newSchedule(Schedule schedule, ScheduleCallback callback);
+    void deleteSchedule(Schedule schedule, ScheduleCallback callback);
+
+    interface ScheduleCallback {
+        void onSuccess();
+        void onSuccess(List<Schedule> scheduleList);
+        void onFailure(String errorMessage);
+    }
+}
